f788259b23c5439f36489e093cfe3ffb9ba99c5f
vinicius_comics <vinicius_comics@hotmail.com>
2019-12-20 18:36:09 -0300

    Implementação dos testes unitários endpoints.

--- eng-zap-challenge-java/src/main/java/br/com/zap/listener/ScheduleCacheClear.java
+++ eng-zap-challenge-java/src/main/java/br/com/zap/listener/ScheduleCacheClear.java
@@ -1,20 +1,21 @@
 package br.com.zap.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import br.com.zap.service.impl.RealEstateAdService;
 
+@Profile("!test")
 @Component
 public class ScheduleCacheClear {
 
 	
 	@Autowired
-	private RealEstateAdService service;
+	private RealEstateAdService service;	
 	
-	
-	@Scheduled(fixedDelay = 1000 * 60 * 30)
+	@Scheduled(fixedDelay = (1000 * 60) * 30)
     public void evictCaches() {		
         service.clearCache();
     }
---
+++ eng-zap-challenge-java/src/test/java/br/com/zap/service/ZapEndPoint.java
@@ -0,0 +1,81 @@
+package br.com.zap.service;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.zap.wrapper.RealEstateAdWrapper;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ZapEndPoint {
+
+	@LocalServerPort
+	private int port;	
+
+	private final String url = "http://localhost:";
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testReturn200() {
+
+		ResponseEntity<RealEstateAdWrapper> resp = new RestTemplate().getForEntity(url + port + "/zap",
+				RealEstateAdWrapper.class);
+
+		Assert.assertEquals(200, resp.getStatusCodeValue());
+
+	}
+
+	@Test
+	public void testReturn404() throws ClientProtocolException, IOException {
+
+		ResponseEntity<RealEstateAdWrapper> resp = new RestTemplate().getForEntity(url + port + "/zap",
+				RealEstateAdWrapper.class);
+
+		Assert.assertEquals(200, resp.getStatusCodeValue());
+
+		HttpUriRequest request = new HttpGet(url + port + "/zap?page=" + (resp.getBody().getTotalPage() + 1) + "&size="
+				+ resp.getBody().getPageSize());
+		
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+		Assert.assertEquals(404, response.getStatusLine().getStatusCode());
+
+	}
+	
+	@Test
+	public void testReturnPaginator() throws ClientProtocolException, IOException {
+
+		ResponseEntity<RealEstateAdWrapper> resp = new RestTemplate().getForEntity(url + port + "/zap",
+				RealEstateAdWrapper.class);
+
+		Assert.assertEquals(200, resp.getStatusCodeValue());
+
+		HttpUriRequest request = new HttpGet(url + port + "/zap?page=" + (resp.getBody().getTotalPage()) + "&size="
+				+ resp.getBody().getPageSize());
+		
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+		Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+
+	}
+}
